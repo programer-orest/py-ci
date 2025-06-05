fd193857eb37cca3274e428408a51570a0029ec0
Orest Yakymiv <akimivorest@gmail.com>
2025-06-05 19:00:03 +0300

    added linter

--- src/app.py
+++ src/app.py
@@ -79,10 +79,14 @@ def external_call():
     try:
         response = requests.get(external_url)
         return Response(
-            f"External call response: {response.text}", status=response.status_code
+            f"External call response: {response.text}",
+            status=response.status_code
         )
     except Exception as e:
-        return Response(f"Error calling external endpoint: {str(e)}", status=500)
+        return Response(
+            f"Error calling external endpoint: {str(e)}",
+            status=500
+        )
 
 
 if __name__ == "__main__":
